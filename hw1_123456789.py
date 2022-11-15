87d2a4c8e61f56cd6fb639ddd396bbabc322cce9
faisalsadi <fesalsadi@gmail.com>
2022-11-15 17:43:37 +0200

    Update hw1_123456789.py
    first

--- hw1_123456789.py
+++ hw1_123456789.py
@@ -1,46 +1,60 @@
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-
 def histImage(im):
-    h = [0] * 256
-    for i in range(im.shape[0]):  # traverses through height of the image
-        for j in range(im.shape[1]):  # traverses through width of the image
-            # print (im[i][j])
-            h[im[i][j]] += 1
+    h=np.zeros(256);
+    dim=np.shape(im)
+    for i in range(dim[0]):
+        for j in range(dim[1]):
+            h[im[i,j]]+=1
     return h
 
 
 def nhistImage(im):
-    nh = histImage(im)
-    pixnum = im.shape[0] * im.shape[1]
-    for i in range(len(nh)):
-        nh[i] /= pixnum
+    nh=histImage(im)
+    div=np.shape(im)[0]*np.shape(im)[1]
+    for i in range(256):
+        nh[i]/=div
     return nh
 
 
 def ahistImage(im):
-    ah = histImage(im)
-    sum = 0
-    for i in range(len(ah)):
-        sum += ah[i]
-        ah[i] = sum
+    ah=histImage(im)
+    for i in range(256):
+        ah[i]+=ah[i-1]
+
     return ah
 
 
 def calcHistStat(h):
-    m = np.matmul(h, np.reshape(np.arange(0, 256), (256, 1))) / sum(h)
-    e = np.matmul(h, np.reshape(pow(np.arange(0, 256) - m, 2), (256, 1))) / sum(h)
+    m = np.matmul(h, np.reshape(np.arange(0, 256), (256, 1)))/sum(h)
+    e = np.matmul(h, np.reshape(pow(np.arange(0, 256)-m, 2), (256 , 1)))/sum(h)
     return m, e
 
 
-def mapImage(im, tm):
-    nim = [[tm[im[i][j]] for i in range(im.shape[0])] for j in range(im.shape[1])]
-    nim=np.clip(nim,0,255)
+def mapImage(im,tm):
+    # nim=[[tm[im[i, j]] for i in range(im.shape[0]) ] for j in range(im.shape[1]) ]
+    # nim=np.clip(nim,0,255)
+    nim=im.copy()
+    for i in range (len(tm)):
+        nim[im==i]=tm[i]
+    nim = np.clip(nim, 0, 255)
+
     return nim
 
-# def histEqualization(im):
-#
-#     return tm
-#
+
+def histEqualization(im):
+    accumulative=ahistImage(im)
+    a=np.zeros(256)
+    for i in range (256):
+        a[i]= accumulative[255]/256
+    goal_hist=np.cumsum(a)
+    tm=np.zeros(256)
+    goal_hist_pointer=0
+    for i  in range (256):
+        while(accumulative[i]>goal_hist[goal_hist_pointer]):
+            goal_hist_pointer+=1
+        tm[i]=goal_hist_pointer
+    return tm
