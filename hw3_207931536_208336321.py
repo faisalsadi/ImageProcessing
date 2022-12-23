ace5d7292a9d16a8a724323fe8731508804cc988
faisalsadi <fesalsadi@gmail.com>
2022-12-23 18:37:26 +0200

    q6 fixed

--- hw3_207931536_208336321.py
+++ hw3_207931536_208336321.py
@@ -19,12 +19,13 @@ def add_SP_noise(im, p):
 
     k = int(p * im.size)  # number of pixels to add noise to
     random_pixels = random.sample(all_pixels, k)
-
+    white=k/2
     # Add noise to the selected pixels
     for x, y in random_pixels:
         # Set the value of the pixel to 0 or 255 at random
-        if random.random() < 0.5:
+        if white > 0:
             sp_noise_im[y, x] = 0
+            white-=1
         else:
             sp_noise_im[y, x] = 255
     return sp_noise_im
@@ -51,7 +52,7 @@ def add_Gaussian_Noise(im, s):
     noise = np.random.normal(0, s, im.shape)
 
     # Add the noise to the image
-    gaussian_noise_im = gaussian_noise_im + noise
+    gaussian_noise_im = gaussian_noise_im + noise.astype(float)
     gaussian_noise_im=np.clip(gaussian_noise_im,0,255)
     return gaussian_noise_im.astype(np.uint8)
 
@@ -63,7 +64,6 @@ def clean_Gaussian_noise(im, radius, maskSTD):
     X, Y = np.meshgrid(np.arange(-radius, radius + 1), np.arange(-radius, radius + 1))
     kernel = np.exp(-(X ** 2 + Y ** 2) / (2 * (maskSTD ** 2)))
     kernel/=np.sum(kernel)
-    print(kernel)
     # Convolve the image with the kernel using scipy.signal.convolve2d
     cleaned_im = convolve2d(cleaned_im.astype(np.float), kernel, mode='same')
 
@@ -73,70 +73,33 @@ def clean_Gaussian_noise(im, radius, maskSTD):
 
 
 def clean_Gaussian_noise_bilateral(im, radius, stdSpatial, stdIntensity):
-    # # bilateral_im = im.copy()
-    # # # TODO: add implementation
-    # #
-    # # return bilateral_im.astype(np.uint8)
-    #
-    # # Create an empty array to store the result
-    # # create the coordinate grid for the neighborhood
-    # X, Y = np.meshgrid(np.arange(im.shape[0]), np.arange(im.shape[1]))
-    #
-    # # initialize the output image
-    # cleanIm = np.zeros_like(im,dtype=float)
-    # gs = np.fromfunction(lambda x, y:
-    #                      np.exp(-(np.divide(pow(x - radius, 2) + pow(y - radius, 2), 2 * pow(stdSpatial, 2)))),
-    #                      (2 * radius + 1, 2 * radius + 1),
-    #                      dtype=np.float)
-    # gs = np.divide(gs, np.sum(gs))
-    # # iterate over the image pixels
-    # for i in range(radius, im.shape[0] - radius):
-    #     for j in range(radius, im.shape[1] - radius):
-    #         # extract the neighborhood window
-    #         window = im[i - radius:i + radius + 1, j - radius:j + radius + 1]
-    #
-    #         # # calculate the spatial weight
-    #         # gs = np.exp(-((X[i - radius:i + radius + 1, j - radius:j + radius + 1] - i) ** 2 + (
-    #         #             Y[i - radius:i + radius + 1, j - radius:j + radius + 1] - j) ** 2) / (2 * stdSpatial ** 2))
-    #         # gs = np.divide(gs, np.sum(gs))
-    #         # calculate the intensity weight
-    #         gi = np.exp(-((window - im[i, j]) ** 2) / (2 * stdIntensity ** 2))
-    #         gi = np.divide(gi, np.sum(gi))
-    #         # combine the weights
-    #
-    #         mask=gs*gi
-    #
-    #         # apply the mask using convolution
-    #         cleanIm[i, j] = np.divide(np.sum(mask * window), np.sum(mask))
-    #
-    # return cleanIm.astype(np.uint8)
-    #
+    # Create the x and y grids for the neighborhood
+    x, y = np.meshgrid(np.arange(-radius, radius + 1), np.arange(-radius, radius + 1))
 
-
-######################################################################
-    # copy image
+    # Calculate the spatial weighting mask
+    gs = np.exp(-(x ** 2 + y ** 2) / (2 * stdSpatial ** 2))
+    gs = gs / gs.sum()
+    # Initialize the filtered image
     bilateral_im = im.copy()
 
-    # creating symmetric gaussian mask the same way as function 4
-    gs = np.fromfunction(lambda x, y:
-                         np.exp(-(np.divide(pow(x - radius, 2) + pow(y - radius, 2), 2 * pow(stdSpatial, 2)))),
-                         (2 * radius + 1, 2 * radius + 1),
-                         dtype=np.float)
-    gs = np.divide(gs, np.sum(gs))
-
-    # creating 2 more masks and re-mapping image
+    # Iterate over the rows and columns of the image
     for i in range(radius, im.shape[0] - radius):
         for j in range(radius, im.shape[1] - radius):
-            window = im[i - radius: i + radius + 1, j - radius: j + radius + 1].astype(np.float)
+            # Extract the neighborhood window
+            window = im[i - radius:i + radius + 1, j - radius:j + radius + 1].astype(np.float)
 
-            gi = np.exp(-np.divide(np.power(np.full((2 * radius + 1, 2 * radius + 1), im[i][j], dtype=float) - window, 2),
-                 2 * pow(stdIntensity, 2)))
-            gi = np.divide(gi, np.sum(gi))
+            # Calculate the intensity weighting mask
+            gi = np.exp(- ( np.power( (window-im[i, j]), 2) / (2 * stdIntensity ** 2)) )
 
-            bilateral_im[i][j] = np.divide(np.sum(gi * gs * window), np.sum(gi * gs))
+            # Normalize the masks so that the sum of their elements is 1
+            gi = gi / gi.sum()
 
-    # return value
+
+            # Calculate the filtered value for the pixel
+            bilateral_im[i, j] = np.divide( np.sum(gi * gs * window) , np.sum(gi * gs) )
+
     return bilateral_im.astype(np.uint8)
+
 
 
 
