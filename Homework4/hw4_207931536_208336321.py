64f975c9bffb1dc6c48be25699b120688fba9cec
faisalsadi <fesalsadi@gmail.com>
2023-01-06 17:55:07 +0200

    HW4

--- Homework4/hw4_207931536_208336321.py
+++ Homework4/hw4_207931536_208336321.py
@@ -25,14 +25,6 @@ dst_points = np.float32([[0, 0],
                          # [int( (255) / 3), 255],
                          [0, 255]]
                         )
-def gkern(l, sig):
-    """\
-    creates gaussian kernel with side length `l` and a sigma of `sig`
-    """
-    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
-    gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
-    kernel = np.outer(gauss, gauss)
-    return kernel / np.sum(kernel)
 
 def cleanImageMean(im, radius, maskSTD):
     ax = np.linspace(-3 / 2., 3 / 2., 4)
