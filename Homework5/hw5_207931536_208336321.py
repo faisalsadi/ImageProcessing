fb906c5fe1f239cc30d05858f2d3076c0d663f39
faisalsadi <fesalsadi@gmail.com>
2023-01-17 18:21:58 +0200

    HoughLinesP

--- Homework5/hw5_207931536_208336321.py
+++ Homework5/hw5_207931536_208336321.py
@@ -52,45 +52,48 @@ def hough_circles(im):
 def hough_lines(im):
     im_l = im.copy()
     # Apply edge detection method on the image
-    #edges = canny(cv2.blur(im,(5,5)))
-    edges = cv2.Canny(cv2.blur(im,(5,5)),120,300,5,L2gradient = True)
+    edges = canny(im_l)
+    #edges = cv2.Canny(cv2.blur(im,(3,3)),200,350,5,L2gradient = True)
     # This returns an array of r and theta values
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 140)
+    #lines = cv2.HoughLines(edges, 1, np.pi / 180, 140)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=10, maxLineGap=250)
 
-
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(im_l, (x1, y1), (x2, y2), (0, 0, 255), 5)
     # The below for loop runs till r and theta values
     # are in the range of the 2d array
-    for r_theta in lines:
-        arr = np.array(r_theta[0], dtype=np.float64)
-        r, theta = arr
-        # Stores the value of cos(theta) in a
-        a = np.cos(theta)
-
-        # Stores the value of sin(theta) in b
-        b = np.sin(theta)
-
-        # x0 stores the value rcos(theta)
-        x0 = a * r
-
-        # y0 stores the value rsin(theta)
-        y0 = b * r
-
-        # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
-        x1 = int(x0 + 1000 * (-b))
-
-        # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
-        y1 = int(y0 + 1000 * (a))
-
-        # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
-        x2 = int(x0 - 1000 * (-b))
-
-        # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
-        y2 = int(y0 - 1000 * (a))
-
-        # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
-        # (0,0,255) denotes the colour of the line to be
-        # drawn. In this case, it is red.
-        cv2.line(im_l, (x1, y1), (x2, y2), (0, 0, 255), 4)
+    # for r_theta in lines:
+    #     arr = np.array(r_theta[0], dtype=np.float64)
+    #     r, theta = arr
+    #     # Stores the value of cos(theta) in a
+    #     a = np.cos(theta)
+    #
+    #     # Stores the value of sin(theta) in b
+    #     b = np.sin(theta)
+    #
+    #     # x0 stores the value rcos(theta)
+    #     x0 = a * r
+    #
+    #     # y0 stores the value rsin(theta)
+    #     y0 = b * r
+    #
+    #     # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
+    #     x1 = int(x0 + 1000 * (-b))
+    #
+    #     # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
+    #     y1 = int(y0 + 1000 * (a))
+    #
+    #     # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
+    #     x2 = int(x0 - 1000 * (-b))
+    #
+    #     # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
+    #     y2 = int(y0 - 1000 * (a))
+    #
+    #     # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
+    #     # (0,0,255) denotes the colour of the line to be
+    #     # drawn. In this case, it is red.
+    #     cv2.line(im_l, (x1, y1), (x2, y2), (0, 0, 255), 4)
     ##
     # for line in lines:
     #     rho, theta = line[0]
